ba17275b48ead2567d7baaafe8bf7149adc0e87e
Goclis Yao <goclisyyh@gmail.com>
2014-04-29 12:51:57 +0800

    添加了access_token的Set与Get，使用pool替换原先的数据库句柄

--- app/redis_db.py
+++ app/redis_db.py
@@ -4,12 +4,14 @@ import calendar
 from datetime import datetime
 import simplejson as json
 from ast import literal_eval
+import time
 
 
 # Redis里的内容
 # key           | description
-# pairing_queue  | 所有的申请配对的用户，配对成功的要剔除出去
+# pairing_queue | 所有的申请配对的用户，配对成功的要剔除出去
 # user_id       | 已配对或在申请配对的用户的信息
+# access_token  | 令牌和时间
 
 # 默认存在数据库 0 以及 pairing_queue 这个key
 
@@ -18,6 +20,17 @@ Config = {
     'port': 6379,
     'db': 0
 }
+
+# Database Helper
+POOL = redis.ConnectionPool(host = Config['host'], port = Config['port'], db = Config['db'])
+def getVariable(key):
+    my_server = redis.Redis(connection_pool = POOL)
+    response = my_server.get(key)
+    return response
+def setVariable(key, value):
+    my_server = redis.Redis(connection_pool = POOL)
+    my_server.set(key, value)
+
 
 # 转化从数据库获取到的空数据
 def convertEmptyResult(result):
@@ -29,13 +42,13 @@ def convertEmptyResult(result):
 # 将用户剔除出申请配对队列
 # 无返回，一般来说不会出现同步异常
 def removeFrompairingQueue(user_id):
-    db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
-    pairing_user_list = literal_eval(convertEmptyResult(db.get('pairing_queue')))
+    # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+    pairing_user_list = literal_eval(convertEmptyResult(getVariable('pairing_queue')))
     try:
         pairing_user_list.remove(user_id)
     except:
         pass
-    db.set('pairing_queue', pairing_user_list)
+    setVariable('pairing_queue', pairing_user_list)
 
 # 用户申请进入聊天
 # 线程会一直执行循环查询数据库以寻找配对的用户
@@ -43,30 +56,30 @@ def removeFrompairingQueue(user_id):
 # 出现同步的异常时删除用户的配对信息，并返回0（这里值得商榷）
 def openChat(user_id):
     try:
-        db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+        # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
         # 添加Key/Value，并加入配对队列
         value = {
             'status': 1,
             'target': ''
         }
-        db.set(user_id, value)
-        old_list = literal_eval(convertEmptyResult(db.get('pairing_queue')))
+        setVariable(user_id, value)
+        old_list = literal_eval(convertEmptyResult(getVariable('pairing_queue')))
         old_list.append(user_id)
-        db.set('pairing_queue', old_list)
+        setVariable('pairing_queue', old_list)
 
         # 为该用户配对
         # 有一个潜在地同步问题。。
         while True:
-            pairing_user_list = literal_eval(convertEmptyResult(db.get('pairing_queue')))
+            pairing_user_list = literal_eval(convertEmptyResult(getVariable('pairing_queue')))
             try:
                 pairing_user_list.remove(user_id) # 去除掉自己
             except:
                 pass
             for target in pairing_user_list:
                 # 检查自己的状态
-                self_value = literal_eval(convertEmptyResult(db.get(user_id)))
+                self_value = literal_eval(convertEmptyResult(getVariable(user_id)))
                 if self_value and self_value['status'] == 1:  # 确实在申请配对
-                    target_value = literal_eval(convertEmptyResult(db.get(target)))
+                    target_value = literal_eval(convertEmptyResult(getVariable(target)))
                     if not target_value:  # 已经不存在了。。
                         removeFrompairingQueue(target)
                     elif target_value['status'] == 1:  # 对方也同样处于配对状态
@@ -74,7 +87,7 @@ def openChat(user_id):
                         # 此处暂时定为用户设置自己的target
                         self_value['status'] = 11
                         self_value['target'] = target
-                        db.set(user_id, self_value) # 更新
+                        setVariable(user_id, self_value) # 更新
                         removeFrompairingQueue(user_id) # 将自己移出申请配对队列
                         return 1
                 else:
@@ -88,8 +101,8 @@ def openChat(user_id):
 # 出现同步的异常时删除用户的配对信息，并返回0（这里值得商榷）
 def getStatus(user_id):
     try:
-        db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
-        user_value = literal_eval(convertEmptyResult(db.get(user_id)))
+        # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+        user_value = literal_eval(convertEmptyResult(getVariable(user_id)))
         if not user_value:
             return 0
         else:
@@ -103,8 +116,8 @@ def getStatus(user_id):
 # 出现同步的异常时返回0
 def getTarget(user_id):
     try:
-        db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
-        user_value = literal_eval(convertEmptyResult(db.get(user_id)))
+        # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+        user_value = literal_eval(convertEmptyResult(getVariable(user_id)))
         if not user_value or \
             (user_value['status'] != 11 and user_value['status'] != 12):
             return 0
@@ -117,13 +130,13 @@ def getTarget(user_id):
 # 出现同步异常（无此用户记录），直接返回0
 def changeStatus(user_id, new_status):
     try:
-        db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
-        user_value = literal_eval(convertEmptyResult(db.get(user_id)))
+        # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+        user_value = literal_eval(convertEmptyResult(getVariable(user_id)))
         if not user_value:
             return 0
         else:
             user_value['status'] = new_status
-            db.set(user_id, user_value)
+            setVariable(user_id, user_value)
     except:
         return 0
 
@@ -133,8 +146,8 @@ def changeStatus(user_id, new_status):
 # 出现同步异常时返回0
 def delChat(user_id):
     try:
-        db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
-        user_value = literal_eval(convertEmptyResult(db.get(user_id)))
+        # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+        user_value = literal_eval(convertEmptyResult(getVariable(user_id)))
         if not user_value:
             return 0
         else:
@@ -143,3 +156,33 @@ def delChat(user_id):
             db.delete(target)
     except:
         return 0        
+
+# 为用户计时n秒
+def countForUser(user_id, n):
+    time.sleep(n)   
+
+    # 在数据库中设置标记位
+    # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+    user_value = literal_eval(convertEmptyResult(getVariable(user_id)))
+    if not user_value:
+        pass
+    else:
+        user_value['counted'] = 1
+        setVariable(user_id, user_value)
+
+# 用户是否之前点过【申请配对】按钮（在已配对的前提下）
+# 
+
+
+# 保存令牌与时间
+def saveStorageToken(token, time):
+    # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+    setVariable('access_token', {
+        'token': token,
+        'date': time
+    })
+
+# 返回令牌和时间，字典格式
+def getStorageToken():
+    # db = redis.Redis(host = Config['host'], port = Config['port'], db = Config['db'])
+    return literal_eval(getVariable('access_token'))
