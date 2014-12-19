af0b4dc7f1c997ace7a65a91759cc9b95d2c3d94
ymlbright <yml_bright@163.com>
2014-12-19 11:29:27 +0800

    reedit

--- mod/units/renew_handler.py
+++ mod/units/renew_handler.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Date    : 2014-06-30 19:28:08
 # @Author  : xindervella@gamil.com yml_bright@163.com
-from tornado.httpclient import HTTPRequest, AsyncHTTPClient, HTTPError
-from config import SERVICE, TIME_OUT
+
 from sqlalchemy.orm.exc import NoResultFound
 from ..models.user import User
-from get_api_return import error_map
+from get_api_return import get_api_return
 import tornado.web
 import tornado.gen
-import urllib
-import json
 
 TEMPLATE = '<center><h1 style="margin-top:30%">{content}</h2></center>'
 
@@ -26,15 +23,9 @@ class RenewHandler(tornado.web.RequestHandler):
         try:
             user = self.db.query(User).filter(User.openid == openid).one()
             
-            params = urllib.urlencode({
-                'uuid': user.uuid,
-                'barcode': barcode
-            })
-            client = AsyncHTTPClient()
-            request = HTTPRequest(SERVICE + 'renew', method='POST', body=params,
-                                  request_timeout=TIME_OUT)
-            response = yield tornado.gen.Task(client.fetch, request)
-            response = json.loads(response.body)
+            data = { 'barcode': barcode }
+            response = get_api_return('renew', user, data)
+
             if response['code'] == 200:
                 if response['content'] == 'success':
                     self.write(TEMPLATE.format(content='续借成功'))
@@ -44,8 +35,6 @@ class RenewHandler(tornado.web.RequestHandler):
                 self.write(response['content'])
         except NoResultFound:
             self.write('access verification fail')
-        except HTTPError as e:
-            self.write(error_map[e.code])
         finally:
             self.finish()
 
