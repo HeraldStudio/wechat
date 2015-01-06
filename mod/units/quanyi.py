a315c176a6ebc6c2046a166e79d6a1e826acc08f
ymlbright <yml_bright@163.com>
2015-01-06 23:21:35 +0800

    reedit

--- mod/units/quanyi.py
+++ mod/units/quanyi.py
@@ -9,14 +9,14 @@ from time import localtime, strftime
 import base64
 
 def quanyi(db, user):
-    msg = u''
+    msg = u'<a href="https://jinshuju.net/f/By3aTK">点我向校权益投诉</a>\n\n'
     try:
         message = db.query(Messsage).filter( Messsage.openid == 'quanyi').one()
         if message.state:
-            msg += u'[%s]\n'%strftime('%Y-%m-%d %H:%M', localtime(message.timestamp))
+            #msg += u'[%s]\n'%strftime('%Y-%m-%d %H:%M', localtime(message.timestamp))
             msg += '%s'%base64.b64decode(message.message).decode('utf8')
     except:
         msg += u'没有什么新的动态'
     finally:
-        msg += u'\n\n<a href="https://jinshuju.net/f/By3aTK">点我向校权益投诉</a>'
+        msg += u'\n\n<a href="https://jinshuju.net/s/VeyDUV">戳我查询投诉结果与动态</a>'
         return msg
